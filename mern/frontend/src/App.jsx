9a9f59c3620e73180d66bc9be15503932a450e5b
Maki Kamiya <109022150+makikamiya@users.noreply.github.com>
2023-09-05 10:20:12 +1200

    Create App.jsx

---
+++ mern/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateArticle from "./pages/CreateArticle";
+import ShowArticle from "./pages/ShowArticle";
+import EditArticle from "./pages/EditArticle";
+import DeleteArticle from "./pages/DeleteArticle";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/articles/create" element={<CreateArticle />} />
+      <Route path="/articles/details/:id" element={<ShowArticle />} />
+      <Route path="/articles/edit/:id" element={<EditArticle />} />
+      <Route path="/articles/delete/:id" element={<DeleteArticle />} />
+    </Routes>
+  );
+};
+
+export default App;
